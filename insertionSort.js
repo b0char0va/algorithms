75915329baa36ad1ff1dbf99017b2caf884a1561
Mona Bocharova <bocharova.mona@gmail.com>
2018-12-26 14:20:12 -0800

    solved insertionSort

---
+++ insertionSort.js
@@ -0,0 +1,22 @@
+//pseudocode
+	//function insertionSort(array) 
+	  // Loop through array
+	  //   Create temp var for current element
+	  //   Create var and set equal to previous element's index
+	  //   Loop backwards while index >= 0 and current element > temp var
+	  //     Set next element equal to current element
+	  //   Set next element equal to temp 
+
+const insertionSort = (arr) => {
+    for(let i = 0; i < arr.length; i++) {
+    	let temp = arr[i];
+    	let j = i-1;
+        for(; j >= 0 && arr[j] > temp; j--) {
+        	arr[j+1] = arr[j];
+        }
+        arr[j+1] = temp;
+    }    
+    return arr;
+};
+
+console.log(insertionSort([6,1,3,5,2,4,9]));
